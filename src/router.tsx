9468c2741fbde1d7c54197a8d1fe4531be8123b7
tazeen04 <atazeenm@gmail.com>
2025-06-01 12:45:51 +0530

    test component set

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Categories from "./pages/Categories";
+
+import LoginPage from "./pages/login/login";
 
 export const route = createBrowserRouter([
     {
@@ -8,7 +9,7 @@ export const route = createBrowserRouter([
         element:<HomePage/>
     },
     {
-        path:"/categories",
-        element:<Categories/>
+        path:"/auth/login",
+        element:<LoginPage/>
     },
 ])
